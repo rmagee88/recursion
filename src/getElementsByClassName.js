f5e6aadc21207016d423c1703318c8e5756544d8
Rich Magee <rmagee88@gmail.com>
2015-01-18 08:42:55 -0800

    Finished getElementsByClassName, syntax checked

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,24 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ){
-  // your code here
+  var nodes = [];
+  // function to find all the nodes recusively
+  function findNodes(node) {
+    // if the class list matches, add the node
+    if (node.classList) {
+      if (node.classList.contains(className)) {
+        nodes.push(node);
+      }
+    }
+    // if there are children, search them
+    if (node.childNodes.length) {
+      for (var i = 0; i < node.childNodes.length; i++) {
+        findNodes(node.childNodes[i]);
+      }
+    }
+  }
+  // call recursive function
+  findNodes(document.body);
+  return nodes;
+
 };
